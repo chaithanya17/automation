7d948d0c0aede3aafd63a1822579dc67e0e0434f
chaithanya17 <chaithanya@bro4u.com>
2019-11-15 16:11:45 +0530

    landing page redesign added  testcases

--- src/stage/landingPageRedesignWeb.java
+++ src/stage/landingPageRedesignWeb.java
@@ -42,7 +42,7 @@ public class landingPageRedesignWeb {
 		Thread.sleep(1000);
 		selectlocation.sendKeys(Keys.DOWN);
 		selectlocation.sendKeys(Keys.RETURN);
-		Thread.sleep(1000);
+		Thread.sleep(1500);
 	}
 	
 	@Test(priority = 2)
@@ -50,11 +50,13 @@ public class landingPageRedesignWeb {
 		
 		driver.findElement(By.xpath(" //label[contains(text(),'Search For Services Here...')]")).click();
 		driver.findElement(By.xpath("//input[@placeholder='Search for services']")).sendKeys("breakdown");
-		Thread.sleep(500);
+		Thread.sleep(800);
 		driver.findElement(By.xpath("//div[@ng-repeat='item in searchServiceCtrl.searchResult']")).click();
+		System.out.println("screen title is : "+ driver.findElement(By.xpath("//span[contains(text(),'Select your Requirement')]")).getText()+"\n");	
+		Thread.sleep(200);
 		List<WebElement> pricechart = driver.findElements(By.xpath("//span[contains(text(),'Price Chart')]"));
 		if (pricechart.size()>0) {
-			System.out.println("price chart is displaying");
+			System.out.println("price chart is displaying"+"\n");
 			Thread.sleep(200);
 			List<WebElement> open_pricechart = driver.findElements(By.xpath("//div[@ng-repeat='eachPC in $ctrl.intScn.intermediateData.price_chart | limitTo:6']"));
 			open_pricechart.get(1).click();
@@ -62,7 +64,7 @@ public class landingPageRedesignWeb {
 			driver.findElement(By.xpath("//i[@class=\"fa fa-times fa-1x\"]")).click();
 		}
 		else {
-			System.out.println("price chart is not displaying");
+			System.out.println("price chart is not displaying"+"\n");
 		}
 		Thread.sleep(800);
 	}
@@ -71,11 +73,11 @@ public class landingPageRedesignWeb {
 	public void dateTimeicon() throws InterruptedException {
 
 		boolean calendericon = driver.findElement(By.xpath("//i[@class='fa fa-calendar mt-5']")).isDisplayed();
-		System.out.println("Calender icon is present : "+calendericon);
+		System.out.println("Calender icon is present : "+calendericon+"\n");
 		
 		boolean timeicon = driver.findElement(By.xpath("//i[@class='fa fa-clock-o mt-9']")).isDisplayed();
-		System.out.println("Time icon is present : "+timeicon);
-		Thread.sleep(500);
+		System.out.println("Time icon is present : "+timeicon+"\n");
+		Thread.sleep(1000);
 		
 		/*driver.findElement(By.xpath("//button[@type='submit']")).click();
 		Thread.sleep(300);
@@ -87,7 +89,6 @@ public class landingPageRedesignWeb {
 	
 	@Test(priority = 4)
 	public void requirementselect() throws InterruptedException {
-		System.out.println("screen title is : "+ driver.findElement(By.xpath("//span[contains(text(),'Select your Requirement')]")).getText());	
 		driver.findElement(By.name("service_type")).click();
 		Thread.sleep(300);
 		List<WebElement> optionSelect = driver.findElements(By.xpath("//md-option[@ng-repeat='option in $ctrl.intScn.intermediateData.option_data track by $index']"));
@@ -108,23 +109,22 @@ public class landingPageRedesignWeb {
 		List<WebElement>selecttime = driver.findElements(By.xpath("//md-option[@ng-if='$ctrl.timeSlots && $ctrl.timeSlots.length']"));
 		selecttime.get(3).click();
 		Thread.sleep(1500);	
-		
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		
 	}
 	
 	@Test(priority = 5)
 	public void provideDetails_screen() throws InterruptedException {
-		System.out.println(driver.findElement(By.xpath("//span[contains(text(),'Provide Details')]")).getText());
+		System.out.println("Next screen title is : "+driver.findElement(By.xpath("//span[contains(text(),'Provide Details')]")).getText()+"\n");
 		Thread.sleep(300);
 		
 		boolean backArrow = driver.findElement(By.xpath("//i[@class='fa fa-long-arrow-left']")).isDisplayed();
-		System.out.println("Back button is present : "+backArrow);
+		System.out.println("Back button is present on Provide details: "+backArrow+"\n");
 		
 		driver.findElement(By.name("name")).sendKeys("Chaithanya");
 		driver.findElement(By.name("streetName")).sendKeys("bro4u, Rajajinagar");
 		driver.findElement(By.name("email")).sendKeys("chai@gmail.com");
-		driver.findElement(By.name("mobile")).sendKeys("8310852676");
+		driver.findElement(By.name("mobile")).sendKeys("8310852678");
 		WebElement locationField = driver.findElement(By.id("location_int"));
 		locationField.sendKeys("koramangala");
 		Thread.sleep(1000);
@@ -139,33 +139,78 @@ public class landingPageRedesignWeb {
 	
 	@Test(priority = 6)
 	public void reveiwRequest_screen() throws InterruptedException {
-		System.out.println(driver.findElement(By.xpath("//span[contains(text(),'Review Your Request')]")).getText());
+		System.out.println("screen title is : "+driver.findElement(By.xpath("//span[contains(text(),'Review Your Request')]")).getText()+"\n");
 		boolean backArrow = driver.findElement(By.xpath("//i[@class='fa fa-long-arrow-left']")).isDisplayed();
-		System.out.println("Back button is present : "+backArrow);
+		System.out.println("Back button is present on Review your request: "+backArrow+"\n");
 		Thread.sleep(1500);
 
 		System.out.println(driver.findElement(By.xpath("//span[contains(text(),'Contact details')]")).getText());
 		List<WebElement> details = driver.findElements(By.xpath("//div[@class=\"service-detail layout-column flex-100\"]"));
-		System.out.println(details.get(1).getText());
+		System.out.println(details.get(1).getText()+"\n");
 			
 	}
 	
 	@Test(priority = 7)
-	public void ConfirmOtp_verify() {
-		System.out.println(driver.findElement(By.xpath("//span[contains(text(),'confirm OTP')]")).getText());
+	public void ConfirmOtp_verify() throws InterruptedException {
+		System.out.println(driver.findElement(By.xpath("//span[contains(text(),'confirm OTP')]")).getText()+"\n");
 		boolean mobileNumber = driver.findElement(By.xpath("//div[@class=\"layout-align-start-center layout-row flex-30\"]")).isDisplayed();
-		System.out.println("mobile number is present : "+mobileNumber);
+		System.out.println("mobile number is present : "+mobileNumber+"\n");
 		
 		boolean changeButton = driver.findElement(By.xpath("//div[@class=\"change-number-btn-container layout-align-start-center layout-row flex-25\"]")).isDisplayed();
-		System.out.println("mobile number is present : "+changeButton);
+		System.out.println("Change button is present : "+changeButton+"\n");
 		
 		boolean resendOtpButton = driver.findElement(By.xpath("//span[contains(text(),'resend otp')]")).isSelected();
-		System.out.println("Resend otp button is enabled : "+resendOtpButton);
-
-		
+		System.out.println("Resend otp button is enabled : "+resendOtpButton+"\n");
+		boolean timer = driver.findElement(By.xpath("//span[@ng-show=\"!$ctrl.isOtpCountDownZero()\"]")).isDisplayed();
+		System.out.println("timer is present : "+timer+"\n");
+		System.out.println("Timer value is : "+driver.findElement(By.xpath("//span[@ng-show=\"!$ctrl.isOtpCountDownZero()\"]")).getText()+"\n");
+		boolean otpTextbox = driver.findElement(By.name("otpEntered")).isDisplayed();
+		System.out.println("Otp textbox is present : "+otpTextbox+"\n");
+		//check change button is working or not
+		Thread.sleep(300);
+		driver.findElement(By.xpath("//div[@class=\"change-number-btn-container layout-align-start-center layout-row flex-25\"]")).click();
+		driver.findElement(By.name("mobile")).sendKeys("7878787879");
+		List<WebElement>BookNowButton = driver.findElements(By.xpath("//button[contains(text(),'Book Now')]"));
+		BookNowButton.get(0).click();
+		Thread.sleep(4000);
 	}
 	
+	@Test(priority = 8)
+	public void verify_backbutton() throws InterruptedException {
+		driver.findElement(By.xpath("//i[@class='fa fa-long-arrow-left']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//i[@class='fa fa-long-arrow-left']")).click();
+		
+		WebElement screenTitle = driver.findElement(By.xpath("//span[contains(text(),'Select your Requirement')]"));
+		if (screenTitle.isDisplayed()) {
+			System.out.println("Back button is working fine"+ "\n");
+		}else {
+			System.out.println("Back button is not working"+"\n");
+		}
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(800);
+		List<WebElement>BookNowButton = driver.findElements(By.xpath("//button[contains(text(),'Book Now')]"));
+		BookNowButton.get(0).click();
+		Thread.sleep(800);	
+	}
 	
+	@Test(priority = 9)
+	public void invalidOtp_resendotp() throws InterruptedException {
+		driver.findElement(By.name("otpEntered")).sendKeys("78788");
+		Thread.sleep(300);
+		driver.findElement(By.xpath("//button[@ng-click=\"$ctrl.proceedLandingBooking()\"]")).click();
+		WebElement toastMessage = driver.findElement(By.xpath("//div[contains(text(),'Please Enter Valid OTP')]"));
+		System.out.println(toastMessage.getText());
+		Thread.sleep(62000);
+		boolean resendOtpButton = driver.findElement(By.xpath("//span[contains(text(),'resend otp')]")).isEnabled();
+		System.out.println("Resend otp button is enabled : "+resendOtpButton);
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(),'resend otp')]")).click();
+		Thread.sleep(200);
+		System.out.println("resend otp toast : "+driver.findElement(By.xpath("//div[contains(text(),'OTP sent to your mobile no.')]")).getText()+"\n");
+		driver.quit();
+	}
 
 	
 }
